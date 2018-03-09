e1cabae67040fe218365dbec43ec565ea4b8c176
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-03-09 16:02:19 +0530

    Create 43withoutstringcat.C

---
+++ 43withoutstringcat.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char a[10],b[20];
+printf("\n enter the first string");
+scanf("%s",&a);
+printf("\n enter the second string");
+scanf("%s",&b);
+printf("%s%s",a,b);
+}
